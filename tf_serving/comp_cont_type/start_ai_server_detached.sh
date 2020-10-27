a956e8122ba7e251b832707d3a92b91b19b8f768
yungyu_choi <ygchoi@zinnotech.com>
2020-10-27 17:25:48 +0900

    modify a little bit

---
+++ tf_serving/comp_cont_type/start_ai_server_detached.sh
@@ -0,0 +1,14 @@
+docker run -t --rm \
+    --detach --name tensorflow-serving \
+    -p 8501:8501 \
+    -v "$(pwd)/models:/models/" tensorflow/serving \
+    --model_config_file=/models/models.config \
+    --model_config_file_poll_wait_seconds=600
+
+
+#docker run -t --rm \
+#    --name tensorflow-serving \
+#    -p 8501:8501 \
+#    -v "$(pwd):/models/" tensorflow/serving \
+#    --model_config_file=/models/models.config \
+#    --model_config_file_poll_wait_seconds=600
